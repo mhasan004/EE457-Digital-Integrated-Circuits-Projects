a30d9db82e9bbaa35f33cb20bea575cd939975b2
Mahmudul Hasan <mhasan0047@gmail.com>
2020-04-19 00:24:37 -0400

    paracitic txt

---
+++ Project_3_MUX_8_to_1_TG_and_CMOS/cmd/MUX_8_1/MUX_8_1.cmd
@@ -0,0 +1,90 @@
+l I0
+l I1
+l I2
+l I3
+l I4
+l I5
+l I6
+l I7
+l S2
+l S1
+l S0
+l out
+
+
+s 20
+h I0
+
+s 35
+l I0
+
+s 40
+h S2
+h I1
+
+s 55
+l I1
+
+s 60
+h S1
+l S2
+h I2
+
+s 75
+l I2
+
+s 80
+h S2
+h I3
+
+s 95
+l I3
+
+s 100
+h S0
+l S1
+l S2
+h I4
+
+s 115
+l I4
+
+s 120
+h S2
+h I5
+
+s 135
+l I5
+
+s 140
+h S1
+l S2
+h I6
+
+s 155
+l I6
+
+s 160
+h S2
+h I7
+
+s 175
+l I7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
